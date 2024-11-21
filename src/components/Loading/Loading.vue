df1a4dd59cb0aa2e55987cead1df671ec33ef2d1
fgomes <fcgomes.92@gmail.com>
2024-11-21 08:53:11 -0300

    (feat) add Loading component

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,55 @@
+<template>
+  <span
+    class="loading"
+    v-if="loading"
+    :class="{
+      [`loading--${size || 'default'}`]: true,
+    }"
+  ></span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  loading: boolean
+  size?: 'default' | 'large'
+}>()
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  aspect-ratio: 1;
+  display: grid;
+  border: 4px solid #0000;
+  border-radius: 50%;
+  border-color: #ccc #0000;
+  animation: loadingAnim 1s infinite linear;
+  &--default {
+    width: 32px;
+  }
+  &--large {
+    width: 64px;
+  }
+}
+.loading::before,
+.loading::after {
+  content: '';
+  grid-area: 1/1;
+  margin: 2px;
+  border: inherit;
+  border-radius: 50%;
+}
+.loading::before {
+  border-color: #f03355 #0000;
+  animation: inherit;
+  animation-duration: 0.5s;
+  animation-direction: reverse;
+}
+.loading::after {
+  margin: 8px;
+}
+@keyframes loadingAnim {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
